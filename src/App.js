433f432ece8223ea8062ef28f4b779c1701a8180
stylianos kouroupas <steliosk9@gmail.com>
2020-01-10 20:37:07 -0600

    app.js render

--- src/App.js
+++ src/App.js
@@ -57,4 +57,29 @@ class App extends Component {
     });
   };
 
+  render() {
+    return (
+      <div className='container'>
+        <div className='row'>
+          <div className='col-10 mx-auto col-md-10 mt-4'>
+            <h3 className='text-center'>Todo Input</h3>
+            <TodoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+              editItem={this.state.editItem}
+            />
+
+            <TodoList
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
